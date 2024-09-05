3fa273f3288147eb11bb7f6cf1c3900de37c3ab9
Frozen-369 <frozenboyarp@gmail.com>
2024-09-05 12:49:17 +0545

    Merge error

--- app/service/auth_service.py
+++ app/service/auth_service.py
@@ -32,7 +32,7 @@ class AuthService:
     async def student_login(email:str, password:str):
         student = await mongodb.collections['student'].find_one({"studentId":email})
         id = str(student['_id'])
-        if student and email == student['student_email'] and verify_password(password,student['password']):
+        if student and email == student['studentId'] and verify_password(password,student['password']):
             access_token = create_access_token(data={"email":email,"id":id, "role":"STUDENT"})
             return {"access_token":access_token, "token_type": "bearer"}
         raise HTTPException(status_code=400, detail="Invalid credentials")
